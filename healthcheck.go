26e4056689691090710b3dbab97a97239fb9f26a
Cem GULER <cem@marti.tech>
2022-04-23 00:36:34 +0300

    rabbit implemented

--- healthcheck.go
+++ healthcheck.go
@@ -20,18 +20,25 @@ func DoHealthCheck(types []string) (string, error) {
 	var err error
 	for _, val := range types {
 		fmt.Println(val)
+		var item ITestableConnection
+		item = nil
 		switch val {
 		case "CASSANDRA":
 			fmt.Println("Trying cassandra")
-			cassandra := new(CassandraConnection)
-			err = cassandra.Connect()
+			item = new(CassandraConnection)
 		case "REDIS":
 			fmt.Println("Trying redis")
-			redis := new(RedisConnection)
-			err = redis.Connect()
+			item = new(RedisConnection)
+		case "RABBITMQ":
+			fmt.Println("Trying rabbitmq")
+			item = new(RabbitConnection)
 		default:
 			fmt.Println("unknown")
 			err = nil
+		}
+
+		if item != nil {
+			err = item.Connect()
 		}
 
 		if err != nil {
